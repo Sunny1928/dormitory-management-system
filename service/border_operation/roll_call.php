c1ba6ff40d9cc6128eaa10bf582bd27fdda245a5
Pinpunyu <pe25617259@gmail.com>
2023-06-01 03:36:55 +0800

    modify roll call sql

--- service/border_operation/roll_call.php
+++ service/border_operation/roll_call.php
@@ -9,6 +9,33 @@
         return $stmt->execute(); 
     }
 
+    //  根據樓長管理的宿舍、年份和狀態查詢點名
+    function roll_call_read_dormitory_year_state($conn , $account, $year, $state){   
+    
+        $sql = "SELECT type FROM border WHERE account = ? AND year = ?";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('si' ,  $account , $year);
+        $stmt->execute();
+        $rel = $stmt->get_result();
+        $dormitory = $rel->fetch_assoc()["type"];
+        mysqli_stmt_close($stmt);
+
+        $dormitory -= 2;
+
+        $sql = "SELECT * FROM roll_call_state_record 
+                JOIN border ON roll_call_state_record.account = border.account 
+                    AND roll_call_state_record.year = border.year 
+                JOIN student ON student.account = border.account 
+                JOIN user ON user.account = student.account 
+                WHERE border.dormitory_id = ? AND border.year = ? AND roll_call_state_record.state = ?";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('iii' ,$dormitory , $year , $state);
+        $stmt->execute();
+        return $stmt->get_result();
+    }
+
     //  根據帳號和年份查詢點名
     function roll_call_read_account_year($conn , $account, $year){   
     
@@ -41,7 +68,7 @@
         return $stmt->get_result();
     }
 
-    //  根據年份查詢暫時出入證
+    //  根據年份查詢點名
     function roll_call_read_year($conn , $year){  
         
         $sql = "SELECT * FROM roll_call_state_record 
@@ -57,7 +84,7 @@
         return $stmt->get_result();
     }
 
-    //  查詢全部暫時出入證
+    //  查詢全部點名
     function roll_call_read_all($conn){  
         
         $sql = "SELECT * FROM roll_call_state_record
@@ -70,7 +97,7 @@
         return $result;
     }
 
-    // 根據id更新state
+    // 根據id更新點名
     function roll_call_update($conn , $access_card_id , $state){  
 
         $sql = "UPDATE roll_call_state_record SET state = ? WHERE roll_call_state_record_id = ?";
@@ -79,7 +106,7 @@
         return $stmt->execute();
     }
 
-    //  根據id刪除暫時出入證
+    //  根據id刪除點名
     function roll_call_delete($conn , $access_card_id){     
 
         $sql = "DELETE FROM roll_call_state_record WHERE roll_call_state_record_id = ?";
