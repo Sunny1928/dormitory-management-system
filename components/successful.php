8c8d76b9076beb1310959585f7a0ae3cad6825be
Sunny <sunnybaby28@gmail.com>
2023-06-13 13:19:18 +0800

    fix bug

--- components/successful.php
+++ components/successful.php
@@ -1,6 +1,6 @@
 <!-- check roll call -->
 <?php
-  if($_GET['status'] == 'success'){
+  if(isset($_GET['status']) && $_GET['status'] == 'success'){
     echo "<div class='alert alert-dismissible fade show alert-success' role='alert' data-mdb-color='success'>
     <strong>操作成功</strong> 
     <button type='button' class='btn-close' data-mdb-dismiss='alert' aria-label='Close'></button>
